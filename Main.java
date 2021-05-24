b9f8c8663a6dc1f70bb67d3603720af5a1510cdf
yesh24 <yeshvanthiniravi@gmail.com>
2021-05-24 08:51:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
-package varun;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hai varun");
-
-	}
-
-}
+public class Main
+ {
+   public static void main(String[] args)
+   {
+     System.out.println("Hai varun");
+   }
+ }
