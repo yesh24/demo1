b4a29740ef85b805476f8beb5e0a735d10055e2e
yesh24 <yeshvanthiniravi@gmail.com>
2021-05-24 08:56:47 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Main
  {
    public static void main(String[] args)
@@ -6,13 +5,3 @@ public class Main
      System.out.println("Hai varun");
    }
  }
-=======
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hai varun");
-
-	}
-
-}
->>>>>>> 00821252f6e2a624efbe5993cda05abe0c7ab786
